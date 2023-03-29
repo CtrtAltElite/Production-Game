c8f73d9af657ef1b66f1d355a540f9a2d94c1a23
reeeeemo <robbie@oxley.ca>
2023-03-28 23:21:31 -0400

    [FIX] mr shark boi work

--- src/LevelOneScene.cpp
+++ src/LevelOneScene.cpp
@@ -40,6 +40,7 @@ void LevelOneScene::Update()
 	if (m_pEnemyPool != nullptr)
 	{
 		m_pEnemyPool->Update();
+		m_pEnemyPool->UpdateTargetPlayer(m_pPlayer);
 	}
 	if (m_pObstaclePool != nullptr) {
 		m_pObstaclePool->Update();
